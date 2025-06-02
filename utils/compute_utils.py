b639bc84ba7b80e4a4c45f1f39cc4b9698fb6e85
Pavel <54832029+onisps@users.noreply.github.com>
2025-06-02 09:47:10 +0700

    [fix] fix odbHistoryOutput_ShellContact.py typo

--- utils/compute_utils.py
+++ utils/compute_utils.py
@@ -165,7 +165,7 @@ def get_history_output_contact(
     fid = open(reqFile, 'wt')
     fid.write('%s,%s' % (pathName, odbFileName))
     fid.close()
-    consoleCommand = 'abaqus cae noGUI=' + str(os.path.join(pathName,'abaqus_scripts/')) + 'odbHistoryOutput_shell_contact.py'
+    consoleCommand = 'abaqus cae noGUI=' + str(os.path.join(pathName,'abaqus_scripts/')) + 'odbHistoryOutput_ShellContact.py'
     outputs_args = os.system(consoleCommand)
 
 def get_history_output(
